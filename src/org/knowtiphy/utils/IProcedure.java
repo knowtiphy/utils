c91a182cec21ea64183f688feefca7597bcb351e
aardy1 <graham@orangedogsoftware.com>
2019-12-28 13:23:41 +1300

    sort of hacky fix for the messages not being sorted by date

--- src/org/knowtiphy/utils/IProcedure.java
+++ src/org/knowtiphy/utils/IProcedure.java
@@ -1,6 +1,17 @@
 package org.knowtiphy.utils;
 
-public interface IProcedure<E extends  Exception>
+public interface IProcedure<E extends Exception>
 {
 	void call() throws E;
+
+	static <T extends Exception> void doAndIgnore(IProcedure<T> procedure)
+	{
+		try
+		{
+			procedure.call();
+		} catch (Exception ex)
+		{
+			//	ignore
+		}
+	}
 }
