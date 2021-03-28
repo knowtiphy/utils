5f42673efb9fa9353d9431bcb70d879418a1f91b
aardy1 <graham@orangedogsoftware.com>
2021-03-28 22:40:26 +1300

    get caldav partially working with the new framework

--- src/org/knowtiphy/utils/IProcedure.java
+++ src/org/knowtiphy/utils/IProcedure.java
@@ -1,17 +1,49 @@
 package org.knowtiphy.utils;
 
-public interface IProcedure<E extends Exception>
-{
-	void call() throws E;
+import java.util.concurrent.Callable;
+import java.util.function.Supplier;
 
-	static void doAndIgnore(IProcedure<?> procedure)
+public interface IProcedure
+{
+	void call() throws Exception;
+
+	static void doAndIgnore(IProcedure procedure)
 	{
 		try
 		{
 			procedure.call();
-		} catch (Exception ex)
+		}
+		catch (Exception ex)
 		{
 			//	ignore
 		}
+	}
+
+	//	wrap a callable into a supplier that turns checked exceptions into runtime exceptions
+	static <T> Supplier<T> wrap(Callable<T> callable)
+	{
+		return () -> {
+			try
+			{
+				return callable.call();
+			}
+			catch (RuntimeException e)
+			{
+				throw e;
+			}
+			catch (Exception e)
+			{
+				throw new RuntimeException(e);
+			}
+		};
+	}
+
+	//	wrap a procedure into a callable which returns null
+	static <T> Supplier<T> wrap(IProcedure procedure)
+	{
+		return wrap(() -> {
+			procedure.call();
+			return null;
+		});
 	}
 }
--- src/org/knowtiphy/utils/JenaUtils.java
+++ src/org/knowtiphy/utils/JenaUtils.java
@@ -1,6 +1,5 @@
 package org.knowtiphy.utils;
 
-import org.apache.jena.datatypes.xsd.XSDDateTime;
 import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ResultSet;
 import org.apache.jena.rdf.model.Literal;
@@ -9,92 +8,62 @@ import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.NodeIterator;
 import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.RDFNode;
-import org.apache.jena.rdf.model.ResIterator;
 import org.apache.jena.rdf.model.Resource;
 import org.apache.jena.rdf.model.Statement;
-import org.apache.jena.rdf.model.StmtIterator;
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.riot.RDFFormat;
 import org.apache.jena.vocabulary.RDF;
 import org.apache.jena.vocabulary.RDFS;
 
-import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Collection;
 import java.util.GregorianCalendar;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.NoSuchElementException;
-import java.util.Set;
 import java.util.function.Consumer;
 import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.logging.Logger;
 
 /**
  * @author graham
+ * <p>
+ * Various utility methods to help with model creation, querying etc
  */
 public class JenaUtils
 {
-	private static final Logger LOGGER = Logger.getLogger(JenaUtils.class.getName());
-	//public final static String RDF_TYPE = RDF.type.toString();
-
-	public static void printModel(StmtIterator it, String pre, PrintWriter pw)
+	//	methods to print a model and convert it to a string
+	public static void printModel(Model model, String pre, StringWriter pw)
 	{
-		while (it.hasNext())
-		{
-			var stmt = it.next();
-			pw.print(pre + " ");
-			pw.println(stmt);
-		}
-	}
-
-	public static void printModel(StmtIterator it, String pre, Predicate<Statement> predicate)
-	{
-		while (it.hasNext())
-		{
-			var stmt = it.next();
-			if (predicate.test(stmt))
-			{
-				System.out.print(pre + " ");
-				System.out.println(stmt);
-			}
-		}
+		pw.append(pre).append("\n");
+		RDFDataMgr.write(pw, model, RDFFormat.NTRIPLES);
 	}
 
 	public static void printModel(Model model, String pre)
 	{
-		printModel(model.listStatements(), pre, x -> true);
+		var sw = new StringWriter();
+		printModel(model, pre, sw);
+		System.out.println(sw);
 	}
 
-//	public static void printModel(Model model, String pre, Predicate<Statement> predicate)
-//	{
-//		printModel(model.listStatements(), pre, predicate);
-//	}
-
-	public static String toString(String pre, Model model, Predicate<Statement> predicate)
+	public static void printModel(Model model)
 	{
-		StringBuilder builder = new StringBuilder();
-		StmtIterator it = model.listStatements();
-		if (it.hasNext())
-		{
-			builder.append("\n");
-		}
-		while (it.hasNext())
-		{
-			var stmt = it.next();
-			if (predicate.test(stmt))
-			{
-				builder.append(pre).append(" ").append(stmt).append("\n");
-			}
-		}
-
-		return builder.toString();
+		var sw = new StringWriter();
+		printModel(model, "", sw);
+		System.out.println(sw);
 	}
 
-	public static boolean checkUnique(Iterator stmts)
+	public static String toString(Model model)
 	{
+		var sw = new StringWriter();
+		RDFDataMgr.write(sw, model, RDFFormat.TURTLE);
+		return sw.toString();
+	}
+
+	public static boolean hasUnique(Model model, String s, String p)
+	{
+		var stmts = listObjectsOfProperty(model, s, p);
 		if (!stmts.hasNext())
 		{
 			return false;
@@ -103,153 +72,7 @@ public class JenaUtils
 		return !stmts.hasNext();
 	}
 
-	public static NodeIterator listObjectsOfProperty(Model model, String subject, String predicate)
-	{
-		return model.listObjectsOfProperty(model.createResource(subject), model.createProperty(predicate));
-	}
-
-	public static RDFNode listObjectsOfPropertyU(Model model, String subject, String predicate)
-	{
-		return listObjectsOfProperty(model, subject, predicate).next();
-	}
-
-	public static ResIterator listSubjectsWithProperty(Model model, String predicate, String object)
-	{
-		return model.listSubjectsWithProperty(model.createProperty(predicate), model.createResource(object));
-	}
-
-	public static Resource listSubjectsWithPropertyU(Model model, String predicate, String object)
-	{
-		return listSubjectsWithProperty(model, predicate, object).next();
-	}
-
-	public static Resource listTypes(Model model, String type)
-	{
-		return listSubjectsWithProperty(model, RDF.type.toString(), type).next();
-	}
-
-	public static boolean getB(RDFNode node)
-	{
-		return node.asLiteral().getBoolean();
-	}
-
-	public static boolean getB(Statement stmt)
-	{
-		return getB(stmt.getObject());
-	}
-
-	public static double getD(RDFNode node)
-	{
-		return node.asLiteral().getDouble();
-	}
-
-	public static double getD(Model model, String subject, String predicate, double defaultValue)
-	{
-		try
-		{
-			return getD(listObjectsOfProperty(model, subject, predicate).next());
-		}
-		catch (NoSuchElementException ex)
-		{
-			return defaultValue;
-		}
-	}
-
-	public static Resource getR(Model model, String subject, String predicate)
-	{
-		return listObjectsOfProperty(model, subject, predicate).next().asResource();
-	}
-
-	public static boolean getB(Model model, String subject, String predicate)
-	{
-		return getB(listObjectsOfProperty(model, subject, predicate).next());
-	}
-
-	public static int getI(Model model, String subject, String predicate, int defaultValue)
-	{
-		try
-		{
-			return getI(listObjectsOfProperty(model, subject, predicate).next());
-		}
-		catch (NoSuchElementException ex)
-		{
-			return defaultValue;
-		}
-	}
-
-	public static boolean has(Model model, String subject, String predicate)
-	{
-		return listObjectsOfProperty(model, subject, predicate).hasNext();
-	}
-
-	public static int getI(RDFNode node)
-	{
-		return node.asLiteral().getInt();
-	}
-
-	public static int getI(Statement stmt)
-	{
-		return getI(stmt.getObject());
-	}
-
-	public static String getS(RDFNode node)
-	{
-		return node.asLiteral().getString();
-	}
-
-	public static String getS(Statement stmt)
-	{
-		return getS(stmt.getObject());
-	}
-
-	public static byte[] getBytes(RDFNode node)
-	{
-		return (byte[]) node.asLiteral().getValue();
-	}
-
-	public static ZonedDateTime getLDT(RDFNode node)
-	{
-		return ZonedDateTime.parse(node.asLiteral().getLexicalForm(),
-				DateTimeFormatter.ISO_DATE_TIME).withZoneSameInstant(ZoneId.systemDefault());
-	}
-
-	public static ZonedDateTime getLD(Statement s)
-	{
-		return getLDT(s.getObject());
-	}
-
-	public static void addSubClass(Model model, String subClass, String superClass)
-	{
-		model.add(R(model, subClass), RDFS.subClassOf, R(model, superClass));
-	}
-
-	public static GregorianCalendar fromDate(ZonedDateTime date)
-	{
-		return GregorianCalendar.from(date);
-	}
-
-	public static ZonedDateTime fromDate(XSDDateTime date)
-	{
-		return ZonedDateTime.ofInstant(date.asCalendar().getTime().toInstant(), ZoneId.systemDefault());
-	}
-
-	public static <T> T single(ResultSet resultSet, Function<QuerySolution, T> f)
-	{
-		return f.apply(resultSet.next());
-	}
-
-	public static <T> Set<T> set(ResultSet resultSet, Function<QuerySolution, T> f)
-	{
-		Set<T> result = new HashSet<>();
-		while (resultSet.hasNext())
-		{
-			result.add(f.apply(resultSet.next()));
-		}
-
-		return result;
-	}
-
-	//	helper methods
+	//	helper methods for creating resources and literals
 
 	public static Resource R(Model model, String uri)
 	{
@@ -266,6 +89,144 @@ public class JenaUtils
 		assert !(value instanceof Literal);
 		return model.createTypedLiteral(value);
 	}
+
+	//	get objects with a given subject and predicate
+	public static NodeIterator listObjectsOfProperty(Model model, String s, String p)
+	{
+		return model.listObjectsOfProperty(R(model, s), P(model, p));
+	}
+
+	public static String listSubjectsOfType(Model model, String type)
+	{
+		return model.listSubjectsWithProperty(RDF.type, R(model, type)).next().toString();
+	}
+
+	public static RDFNode listObjectsOfPropertyU(Model model, String s, String p)
+	{
+		return listObjectsOfProperty(model, s, p).next();
+	}
+
+	//	methods to extract objects from models
+	//	These methods all assume the objects exist
+
+	//	get the o from (s, p, o) as a resource (assumes that the object exists)
+	public static Resource getOR(Model model, String s, String p)
+	{
+		return listObjectsOfProperty(model, s, p).next().asResource();
+	}
+
+	//	get the o from (s, p, o) as a literal (assumes that the object exists)
+	public static Literal getOL(Model model, String s, String p)
+	{
+		return listObjectsOfProperty(model, s, p).next().asLiteral();
+	}
+
+	//	get the o from (s, p, o) as a literal and convert it into some result type
+	public static <T> T getOL(Model model, String s, String p, Function<Literal, T> converter)
+	{
+		return converter.apply(getOL(model, s, p));
+	}
+
+	//	get the o from (s, p, o) as a literal and convert it into some result type
+	//	if o isn't there then return a default value
+	public static <T> T getOL(Model model, String s, String p,
+							  Function<Literal, T> converter, T defaultValue)
+	{
+		try
+		{
+			return getOL(model, s, p, converter);
+		}
+		catch (NoSuchElementException ex)
+		{
+			return defaultValue;
+		}
+	}
+
+	//	methods to extract literal values from statements (used in Peer updaters)
+	//	These methods all assume the literal exists -- if not a NoSuchElementException is
+	//	thrown by Jena
+
+	public static boolean getB(Statement stmt)
+	{
+		return stmt.getObject().asLiteral().getBoolean();
+	}
+
+	public static int getI(Statement stmt)
+	{
+
+		return stmt.getObject().asLiteral().getInt();
+	}
+
+	public static String getS(Statement stmt)
+	{
+
+		return stmt.getObject().asLiteral().getString();
+	}
+
+	public static ZonedDateTime getDate(Statement s)
+	{
+		return ZonedDateTime.parse(s.getObject().asLiteral().getLexicalForm(),
+				DateTimeFormatter.ISO_DATE_TIME).withZoneSameInstant(ZoneId.systemDefault());
+	}
+
+	//	methods to extract literal values from models, and if they don't exist, return
+	//	default values
+
+	public static Boolean getB(Model model, String s, String p)
+	{
+		return getOL(model, s, p, Literal::getBoolean);
+	}
+
+	public static double getOD(Model model, String s, String p, double defaultValue)
+	{
+		return getOL(model, s, p, Literal::getDouble, defaultValue);
+	}
+
+	public static String getS(Model model, String s, String p)
+	{
+		return getOL(model, s, p, Literal::getString);
+	}
+
+	//	methods to extract objects from query solutions
+
+	public static ZonedDateTime getDate(QuerySolution soln, String var)
+	{
+		return ZonedDateTime.parse(soln.get(var).asLiteral().getLexicalForm(),
+				DateTimeFormatter.ISO_DATE_TIME).withZoneSameInstant(ZoneId.systemDefault());
+	}
+
+	//	apply a function over a list of objects
+	public static void apply(Model model, String s, String p, Consumer<RDFNode> f)
+	{
+		JenaUtils.listObjectsOfProperty(model, s, p).forEachRemaining(f);
+	}
+
+	//	collect a collection of objects from model triples
+	public static <T> Collection<T> apply(Model model, String id, String prop,
+										  Function<RDFNode, T> f, Collection<T> result)
+	{
+		apply(model, id, prop, x -> result.add(f.apply(x)));
+		return result;
+	}
+
+
+	//	methods to extract values from result sets
+	//	TODO -- not sure these are really necessary
+	//	TODO -- they are also wrong since they don't close the result sets
+
+	public static <T> T single(ResultSet resultSet, Function<QuerySolution, T> f)
+	{
+		return f.apply(resultSet.next());
+	}
+
+	//	here because its used in conjunction with fetching RDF data
+	public static GregorianCalendar fromDate(ZonedDateTime date)
+	{
+		return GregorianCalendar.from(date);
+	}
+
+
+	//	methods for adding statements to models
 
 	public static void addType(Model model, String s, String type)
 	{
@@ -285,37 +246,46 @@ public class JenaUtils
 
 	public static <T> void addDPN(Model model, String s, String p, T o)
 	{
+		//	ignore o if its null
 		if (o != null)
 		{
 			addDP(model, s, p, o);
 		}
 	}
 
-	public static Model createRDFSModel(Model base, Map<String, String> subClasses)
+	//	methods to help make RDFS inferencing models
+
+	public static Model addSubClasses(Model model, Map<String, String> subClasses)
 	{
-		var model = ModelFactory.createRDFSModel(base);
-		JenaUtils.addSubClasses(model, subClasses);
+		subClasses.forEach((sub, sup) ->
+				model.add(R(model, sub), RDFS.subClassOf, R(model, sup)));
 		return model;
 	}
 
-	public static Model createRDFSModel(Map<String, String> subClasses)
+	public static Model createRDFSModel(Model base, Map<String, String> subClasses)
 	{
-		return createRDFSModel(ModelFactory.createDefaultModel(), subClasses);
-	}
-	public static void addSubClasses(Model model, Map<String, String> subClasses)
-	{
-		subClasses.forEach((sub, sup) -> addSubClass(model, sub, sup));
-	}
-
-	public static void collect(Model model, String id, String prop, Consumer<RDFNode> f)
-	{
-		JenaUtils.listObjectsOfProperty(model, id, prop).forEachRemaining(f);
-	}
-
-	public static <T> Collection<T> collect(Collection<T> result, Model model,
-										 String id, String prop, Function<RDFNode, T> f)
-	{
-		JenaUtils.listObjectsOfProperty(model, id, prop).forEachRemaining(x -> result.add(f.apply(x)));
-		return result;
+		return JenaUtils.addSubClasses(ModelFactory.createRDFSModel(base), subClasses);
 	}
 }
+
+
+//	get subjects with a given predicate and object
+//	public static ResIterator listSubjectsWithProperty(Model model, String predicate, String object)
+//	{
+//		return model.listSubjectsWithProperty(P(model, predicate), R(model, object));
+//	}
+//
+//	public static ResIterator listTypes(Model model, String type)
+//	{
+//		return model.listSubjectsWithProperty(RDF.type, R(model, type));
+//	}
+//
+//	public static ZonedDateTime fromDate(XSDDateTime date)
+//	{
+//		return ZonedDateTime.ofInstant(date.asCalendar().getTime().toInstant(), ZoneId.systemDefault());
+//	}
+
+//	public static byte[] getBytes(RDFNode node)
+//	{
+//		return (byte[]) node.asLiteral().getValue();
+//	}
